53f84795a7f06347388aec7ef271a4631bb8d2d3
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2022-12-06 17:17:13 +0530

    Update predict.py

--- Tabs/predict.py
+++ Tabs/predict.py
@@ -17,7 +17,7 @@ def app(df, X, y):
     st.markdown(
         """
             <p style="font-size:25px">
-                This app uses <b style="color:green">Decision Tree Classifier</b> for the Cardiac Disease Prediction.
+                This app uses <b style="color:green">Decision Tree Classifier</b> for the Renal Disease Prediction.
             </p>
         """, unsafe_allow_html=True)
     

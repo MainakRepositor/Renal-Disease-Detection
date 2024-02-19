cc51dc33ff686cc192d8d23cc16de5b5e906a046
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2024-02-19 05:47:00 +0530

    Update predict.py

--- Tabs/predict.py
+++ Tabs/predict.py
@@ -59,13 +59,14 @@ def app(df, X, y):
     df3.columns=["Bp","Al","Su","Bu","Sc","Sod","Pot","Hemo","Wbcc","Rbcc"]
     st.dataframe(df3)
 
-    score = prediction, score = predict(X, y, features)
+    
 
     # Create a button to predict
     if st.button("Predict"):
         # Get prediction and model score
         # prediction, score = predict(X, y, features)
         # score = 0.9876 
+        score = prediction, score = predict(X, y, features)
      
 
         if(Bp > 125):
